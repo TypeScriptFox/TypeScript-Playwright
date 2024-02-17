83843907b578083fe6ac43d3b7663b05ea413ecd
usagimadokii@gmail.com <usagimadokii@gmail.com>
2024-02-17 17:04:16 -0600

    Add test for entering numbers in username and password fields

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -10,3 +10,21 @@ test("Username and Password Fields are Visible", async ({ page }) => {
   await (await loginPage.getUserNameField()).waitFor({ state: "visible" });
   await (await loginPage.getPasswordField()).waitFor({ state: "visible" });
 });
+
+test("Username and Password Fields can have numbers entered into them", async ({
+  page,
+}) => {
+  const loginPage = new PlaywrightHomePage(page);
+
+  await loginPage.navigate();
+
+  // Expect the Logo of the page to Exist
+  await (await loginPage.getUserNameField()).waitFor({ state: "visible" });
+  await (await loginPage.getPasswordField()).waitFor({ state: "visible" });
+
+  await (await loginPage.getUserNameField()).fill("123");
+  await (await loginPage.getPasswordField()).fill("123");
+
+  expect(await (await loginPage.getUserNameField()).inputValue()).toBe("123");
+  expect(await (await loginPage.getPasswordField()).inputValue()).toBe("123");
+});
